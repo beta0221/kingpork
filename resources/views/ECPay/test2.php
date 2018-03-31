76e79b42b2071c9a5b62c985bf50d941e0a2f99f
beta0221 <beta0221@gmail.com>
2018-03-31 22:07:49 +0800

    123

--- resources/views/ECPay/test2.php
+++ resources/views/ECPay/test2.php
@@ -3,12 +3,12 @@
 $price = Request::get('price');
 $bill_id = Request::get('bill_id');
 
-if(isset($_GET['price'])){
-	$price = $_GET["price"];	
-}
-if(isset($_GET['bill_id'])){
-	$bill_id = $_GET["bill_id"];	
-}
+// if(isset($_GET['price'])){
+// 	$price = $_GET["price"];	
+// }
+// if(isset($_GET['bill_id'])){
+// 	$bill_id = $_GET["bill_id"];	
+// }
 
 
 echo $price;
