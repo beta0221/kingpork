69474b00b8ea571139856934245cad9a114d29f3
beta0221 <beta0221@gmail.com>
2018-03-31 22:09:43 +0800

    123

--- resources/views/ECPay/test.blade.php
+++ resources/views/ECPay/test.blade.php
@@ -6,11 +6,9 @@
 /**
 *   ATM產生訂單範例
 */
-$price = Request::get('price');
-$bill_id = Request::get('bill_id');
-	// $price = $_GET['price'];
-	// $bill_id = $_GET['bill_id'];
     
+    //載入SDK(路徑可依系統規劃自行調整)
+ 
     try {
         
     	$obj = new ECPay_AllInOne();
@@ -22,26 +20,38 @@ $bill_id = Request::get('bill_id');
         $obj->MerchantID  = '2000132';                                                     //測試用MerchantID，請自行帶入ECPay提供的MerchantID
         $obj->EncryptType = '1';                                                           //CheckMacValue加密類型，請固定填入1，使用SHA256加密
         //基本參數(請依系統規劃自行調整)
-        // $MerchantTradeNo = "Test".time() ;
+        $MerchantTradeNo = "Test".time() ;
         $obj->Send['ReturnURL']         = "http://www.ecpay.com.tw/receive.php" ;    //付款完成通知回傳的網址
-        $obj->Send['MerchantTradeNo']   = $bill_id;                          //訂單編號
-        $obj->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');               //交易時間
-        $obj->Send['TotalAmount']       = $price;                            //交易金額
-        $obj->Send['TradeDesc']         = "ECPay-ATM" ;                      //交易描述
-        $obj->Send['ChoosePayment']     = ECPay_PaymentMethod::ATM ;         //付款方式:ATM
+        $obj->Send['MerchantTradeNo']   = $MerchantTradeNo;                          //訂單編號
+        $obj->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');                       //交易時間
+        $obj->Send['TotalAmount']       = 2000;                                      //交易金額
+        $obj->Send['TradeDesc']         = "good to drink" ;                          //交易描述
+        $obj->Send['ChoosePayment']     = ECPay_PaymentMethod::ATM ;                 //付款方式:ATM
         //訂單的商品資料
-        array_push($obj->Send['Items'], 
-        	array(
-        		'Name' => 'test',
-        		'Price' => (int)$price,
-                'Currency' => "元",
-                'Quantity' => (int) "0",
-                'URL' => "dedwed"
-            ));
+        array_push($obj->Send['Items'], array('Name' => "歐付寶黑芝麻豆漿", 'Price' => (int)"2000",
+                   'Currency' => "元", 'Quantity' => (int) "1", 'URL' => "dedwed"));
         //ATM 延伸參數(可依系統需求選擇是否代入)
         $obj->SendExtend['ExpireDate'] = 3 ;     //繳費期限 (預設3天，最長60天，最短1天)
         $obj->SendExtend['PaymentInfoURL'] = ""; //伺服器端回傳付款相關資訊。
-        
+        # 電子發票參數
+         /*
+        $obj->Send['InvoiceMark'] = ECPay_InvoiceState::Yes;
+        $obj->SendExtend['RelateNumber'] = "Test".time();
+        $obj->SendExtend['CustomerEmail'] = 'test@ecpay.com.tw';
+        $obj->SendExtend['CustomerPhone'] = '0911222333';
+        $obj->SendExtend['TaxType'] = ECPay_TaxType::Dutiable;
+        $obj->SendExtend['CustomerAddr'] = '台北市南港區三重路19-2號5樓D棟';
+        $obj->SendExtend['InvoiceItems'] = array();
+        // 將商品加入電子發票商品列表陣列
+        foreach ($obj->Send['Items'] as $info)
+        {
+            array_push($obj->SendExtend['InvoiceItems'],array('Name' => $info['Name'],'Count' =>
+                $info['Quantity'],'Word' => '個','Price' => $info['Price'],'TaxType' => ECPay_TaxType::Dutiable));
+        }
+        $obj->SendExtend['InvoiceRemark'] = '測試發票備註';
+        $obj->SendExtend['DelayDay'] = '0';
+        $obj->SendExtend['InvType'] = ECPay_InvType::General;
+        */
         //產生訂單(auto submit至ECPay)
         $html = $obj->CheckOut();
         echo $html;
