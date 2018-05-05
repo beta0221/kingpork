a0152fadc45ef9508365ade9adfef1e843ef8439
beta0221 <beta0221@gmail.com>
2018-05-06 00:24:57 +0800

    123132

--- public/creditPaied.php
+++ public/creditPaied.php
@@ -1,12 +1,12 @@
 <?php
 
-echo $_POST['status'];
-echo $_POST['errcode'];
-echo $_POST['authCode'];
-echo $_POST['authAmt'];
-echo $_POST['lidm'];
-echo $_POST['xid'];
-echo $_POST['merID'];
-echo $_POST['Last4digitPAN'];
-echo $_POST['errDesc'];
-echo $_POST['checkValue'];
+echo $_POST['status'].'<br>';
+echo $_POST['errcode'].'<br>';
+echo $_POST['authCode'].'<br>';
+echo $_POST['authAmt'].'<br>';
+echo $_POST['lidm'].'<br>';
+echo $_POST['xid'].'<br>';
+echo $_POST['merID'].'<br>';
+echo $_POST['Last4digitPAN'].'<br>';
+echo $_POST['errDesc'].'<br>';
+echo $_POST['checkValue'].'<br>';
