658598e4a461a6cc2ae1c4a420aecef86d67b4d8
beta0221 <beta0221@gmail.com>
2018-05-10 01:34:45 +0800

    13123

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>金園排骨 @yield('title')</title>
 {{Html::style('css/reset.css')}}
 {{Html::style('css/bootstrap/bootstrap.min.css')}}
