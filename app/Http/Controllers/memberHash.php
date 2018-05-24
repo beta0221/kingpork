40f8c3af12e13410f14a7bbb31799b89f0e05187
beta0221 <beta0221@gmail.com>
2018-05-24 20:10:36 +0800

    123

--- app/Http/Controllers/memberHash.php
+++ app/Http/Controllers/memberHash.php
@@ -11,15 +11,15 @@ class memberHash extends Controller
 	public function memberHash()
 	{
 
-		//584~841重複Hash要全部重新匯入＆Hash
-
-		$users = User::where('id','>=',818)->where('id','<=',841)->get();
-		foreach ($users as $user) {
-			$user->password=Hash::make($user->password);
-			$user->save();
-		}
 		
-		return('Hashing has done');
+
+		// $users = User::where('id','>=',818)->where('id','<=',841)->get();
+		// foreach ($users as $user) {
+		// 	$user->password=Hash::make($user->password);
+		// 	$user->save();
+		// }
+		
+		return('404 not found');
 	}
     
 }
