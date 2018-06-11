b42a7221e70bb4bfb035059015e07ed108338f4f
beta0221 <beta0221@gmail.com>
2018-06-11 21:45:17 +0800

    123

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 		$('.arrow-up').css('left','83.33333%');
 		$('.shop-now-img').removeClass('shop-now-img');
 		$('#story-3-box img').addClass('shop-now-img');
-		if (story2_ismap == 0) {
+		if (story3_ismap == 0) {
 			$('#story-3').append('<div class="'+'google-map">'+'<iframe src="'+'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.2203398053202!2d121.3662263148874!3d25.060519983959885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a73adf8e04d1%3A0x2f3929d61cb8ab77!2zMzMz5qGD5ZyS5biC6b6c5bGx5Y2A5b6p6IiI6KGXNS036Jmf!5e0!3m2!1szh-TW!2stw!4v1528713158239" '+'frameborder="'+'0"'+' allowfullscreen></iframe></div>');
 			story3_ismap = 1;
 		}
