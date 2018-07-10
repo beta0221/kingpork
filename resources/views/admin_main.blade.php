45e4e650ec670f5c7f9956f758e04c00d1cb222c
beta0221 <beta0221@gmail.com>
2018-07-10 22:35:20 +0800

    0710

--- resources/views/admin_main.blade.php
+++ resources/views/admin_main.blade.php
@@ -123,6 +123,7 @@
   <ul class="topBar_left">
     <li><a href="/" target="_blank"><img src="{{asset('images/admin_haveLook.png')}}">瀏覽網頁</a></li>
     <li><a href="https://analytics.google.com/analytics/web/?hl=zh-TW&pli=1#/realtime/rt-overview/a121883818w179867443p178085533" target="_blank"><img src="{{asset('images/admin_GA.png')}}">GA分析</a></li>
+    <li><a href="https://my.vultr.com/subs/?SUBID=14512165" target="_blank"><img src="{{asset('images/admin_server.png')}}">伺服器</a></li>
   </ul>
   <ul class="topBar_right">
     <li>{{Auth::user()->job_title}}</li>
