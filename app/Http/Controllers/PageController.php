133042c23f2a62358438064d69ba74ff78e89d1f
beta0221 <beta0221@gmail.com>
2018-11-21 02:22:05 +0800

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -51,45 +51,45 @@ class PageController extends Controller
         //validate from google
         $reCaptcha = new ReCaptcha($secret);
 
-        if ($request->input('g-recaptcha-response')) {
-            $response = $reCaptcha->verifyResponse(
-                request()->ip(),
-                $request->input('g-recaptcha-response')
-            );
-        }
+        // if ($request->input('g-recaptcha-response')) {
+        //     $response = $reCaptcha->verifyResponse(
+        //         request()->ip(),
+        //         $request->input('g-recaptcha-response')
+        //     );
+        // }
 
-        if ($response != null && $response->success){
+        // if ($response != null && $response->success){
 
-        }
+        // }
 
 
-        $this->validate($request,[
-            'name'=>'required',
-            'email'=>'required|E-mail',
-            'title'=>'required',
-            'text'=>'required'
-        ]);
+        // $this->validate($request,[
+        //     'name'=>'required',
+        //     'email'=>'required|E-mail',
+        //     'title'=>'required',
+        //     'text'=>'required'
+        // ]);
 
-        $contact = new Contact;
-        $contact->name = $request->name;
-        $contact->email = $request->email;
-        $contact->title = $request->title;
-        $contact->message = $request->text;
-        $contact->save();
+        // $contact = new Contact;
+        // $contact->name = $request->name;
+        // $contact->email = $request->email;
+        // $contact->title = $request->title;
+        // $contact->message = $request->text;
+        // $contact->save();
 
-        $data =[
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'title'=>$request->title,
-            'text'=>$request->text
-        ];
-        Mail::send('emails.messageNotification',$data,function($message) use ($data){
-            $message->from('kingpork80390254@gmail.com','金園排骨-客服通知');
-            $message->to('beta0221@gmail.com');
-            $message->subject($data['title']);
-        });
-
-        Session::flash('success','訊息已成功送出，我們將會儘速回覆您。');
+        // $data =[
+        //     'name'=>$request->name,
+        //     'email'=>$request->email,
+        //     'title'=>$request->title,
+        //     'text'=>$request->text
+        // ];
+        // Mail::send('emails.messageNotification',$data,function($message) use ($data){
+        //     $message->from('kingpork80390254@gmail.com','金園排骨-客服通知');
+        //     $message->to('beta0221@gmail.com');
+        //     $message->subject($data['title']);
+        // });
+        Session::flash('success',$request->input('g-recaptcha-response'));
+        // Session::flash('success','訊息已成功送出，我們將會儘速回覆您。');
         return view('pages.contact');
 
     }
