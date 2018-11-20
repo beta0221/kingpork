cd3b52827584765b510a48ae8a4115069a46e47c
beta0221 <beta0221@gmail.com>
2018-11-21 03:12:55 +0800

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,7 +8,6 @@ use App\Banner;
 use App\Contact;
 use Session;
 use Mail;
-use App\Libraries\Recaptchalib;
 
 class PageController extends Controller
 {
@@ -46,10 +45,14 @@ class PageController extends Controller
 
     public function contactUs(Request $request){
 
-        // $sender_name = stripslashes($_POST["sender_name"]);
-        // $sender_email = stripslashes($_POST["sender_email"]);
-        // $sender_message = stripslashes($_POST["sender_message"]);
-        // $response = $request->input('g-recaptcha-response');
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|E-mail',
+            'title'=>'required',
+            'text'=>'required'
+        ]);
+
+        //recaptcha
         $url = 'https://www.google.com/recaptcha/api/siteverify';
         $data = array(
             'secret' => '6LfOZnoUAAAAAFNdAX43Z17487emgfmW5r1Rj9CQ',
@@ -65,58 +68,34 @@ class PageController extends Controller
         $context  = stream_context_create($options);
         $verify = file_get_contents($url, false, $context);
         $captcha_success=json_decode($verify);
-        if ($captcha_success->success==false) {
-            // echo "<p>You are a bot! Go away!</p>";
-            return response()->json($captcha_success);
-        } else if ($captcha_success->success==true) {
-            // echo "<p>You are not not a bot!</p>";
-            return response()->json($captcha_success);
-        }
-        // $secret = "6LfOZnoUAAAAAFNdAX43Z17487emgfmW5r1Rj9CQ";
-        // $response = null;
-        // //validate from google
-        // $reCaptcha = new ReCaptcha($secret);
-
-        // if ($request->input('g-recaptcha-response')) {
-        //     $response = $reCaptcha->verifyResponse(
-        //         request()->ip(),
-        //         $request->input('g-recaptcha-response')
-        //     );
-        // }
-
-        // if ($response != null && $response->success){
-
-        // }
-
-
-        // $this->validate($request,[
-        //     'name'=>'required',
-        //     'email'=>'required|E-mail',
-        //     'title'=>'required',
-        //     'text'=>'required'
-        // ]);
-
-        // $contact = new Contact;
-        // $contact->name = $request->name;
-        // $contact->email = $request->email;
-        // $contact->title = $request->title;
-        // $contact->message = $request->text;
-        // $contact->save();
-
-        // $data =[
-        //     'name'=>$request->name,
-        //     'email'=>$request->email,
-        //     'title'=>$request->title,
-        //     'text'=>$request->text
-        // ];
-        // Mail::send('emails.messageNotification',$data,function($message) use ($data){
-        //     $message->from('kingpork80390254@gmail.com','金園排骨-客服通知');
-        //     $message->to('beta0221@gmail.com');
-        //     $message->subject($data['title']);
-        // });
         
-        // Session::flash('success','訊息已成功送出，我們將會儘速回覆您。');
-        // return view('pages.contact');
+        if ($captcha_success->success==false) {
+            return response("Go away Robot!");
+        } else if ($captcha_success->success==true) {
+        
+            $contact = new Contact;
+            $contact->name = $request->name;
+            $contact->email = $request->email;
+            $contact->title = $request->title;
+            $contact->message = $request->text;
+            $contact->save();
+
+            $data =[
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'title'=>$request->title,
+                'text'=>$request->text
+            ];
+            Mail::send('emails.messageNotification',$data,function($message) use ($data){
+                $message->from('kingpork80390254@gmail.com','金園排骨-客服通知');
+                $message->to('beta0221@gmail.com');
+                $message->subject($data['title']);
+            });
+            
+            Session::flash('success','訊息已成功送出，我們將會儘速回覆您。');
+            return view('pages.contact');
+
+        }
         
 
     }
@@ -138,99 +117,3 @@ class PageController extends Controller
 
 }
 
-class ReCaptchaResponse
-{
-    public $success;
-    public $errorCodes;
-}
-class ReCaptcha
-{
-    private static $_signupUrl = "https://www.google.com/recaptcha/admin";
-    private static $_siteVerifyUrl =
-        "https://www.google.com/recaptcha/api/siteverify?";
-    private $_secret;
-    private static $_version = "php_1.0";
-    /**
-     * Constructor.
-     *
-     * @param string $secret shared secret between site and ReCAPTCHA server.
-     */
-    function ReCaptcha($secret)
-    {
-        if ($secret == null || $secret == "") {
-            die("To use reCAPTCHA you must get an API key from <a href='"
-                . self::$_signupUrl . "'>" . self::$_signupUrl . "</a>");
-        }
-        $this->_secret=$secret;
-    }
-    /**
-     * Encodes the given data into a query string format.
-     *
-     * @param array $data array of string elements to be encoded.
-     *
-     * @return string - encoded request.
-     */
-    private function _encodeQS($data)
-    {
-        $req = "";
-        foreach ($data as $key => $value) {
-            $req .= $key . '=' . urlencode(stripslashes($value)) . '&';
-        }
-        // Cut the last '&'
-        $req=substr($req, 0, strlen($req)-1);
-        return $req;
-    }
-    /**
-     * Submits an HTTP GET to a reCAPTCHA server.
-     *
-     * @param string $path url path to recaptcha server.
-     * @param array  $data array of parameters to be sent.
-     *
-     * @return array response
-     */
-    private function _submitHTTPGet($path, $data)
-    {
-        $req = $this->_encodeQS($data);
-        $response = file_get_contents($path . $req);
-        return $response;
-    }
-    /**
-     * Calls the reCAPTCHA siteverify API to verify whether the user passes
-     * CAPTCHA test.
-     *
-     * @param string $remoteIp   IP address of end user.
-     * @param string $response   response string from recaptcha verification.
-     *
-     * @return ReCaptchaResponse
-     */
-    public function verifyResponse($remoteIp, $response)
-    {
-        // Discard empty solution submissions
-        if ($response == null || strlen($response) == 0) {
-            $recaptchaResponse = new ReCaptchaResponse();
-            $recaptchaResponse->success = false;
-            $recaptchaResponse->errorCodes = 'missing-input';
-            return $recaptchaResponse;
-        }
-        $getResponse = $this->_submitHttpGet(
-            self::$_siteVerifyUrl,
-            array (
-                'secret' => $this->_secret,
-                'remoteip' => $remoteIp,
-                'v' => self::$_version,
-                'response' => $response
-            )
-        );
-        $answers = json_decode($getResponse, true);
-        
-        // $recaptchaResponse = new ReCaptchaResponse();
-        // if (trim($answers ['success']) == true) {
-        //     $recaptchaResponse->success = true;
-        // } else {
-        //     $recaptchaResponse->success = false;
-        //     $recaptchaResponse->errorCodes = $answers [error-codes];
-        // }
-        // return $recaptchaResponse;
-        return $answers;
-    }
-}
