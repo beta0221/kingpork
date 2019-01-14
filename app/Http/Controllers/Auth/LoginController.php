13ae4965256e23b9f73076384620a70ca15b37ba
beta0221 <beta0221@gmail.com>
2019-01-14 16:14:08 +0800

    login redirect to previous

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
+use Session;
 
 class LoginController extends Controller
 {
@@ -40,6 +41,22 @@ class LoginController extends Controller
         // $this->middleware('guest')->except('logout');
         $this->middleware('guest',['except'=>['logout','userLogout']]);
     }
+
+
+    //override start
+    public function showLoginForm()
+    {
+        session(['link' => url()->previous()]);
+        return view('auth.login');
+    }
+    protected function authenticated()
+    {
+        return redirect(session('link'));
+    }
+    //override end
+
+
+
 
     public function userLogout()
     {
