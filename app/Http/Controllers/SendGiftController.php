081ca74ffcd4759d73aaef026fdc21e5d80da067
beta0221 <beta0221@gmail.com>
2022-06-07 21:36:45 +0800

    禮盒暫停供應

--- app/Http/Controllers/SendGiftController.php
+++ app/Http/Controllers/SendGiftController.php
@@ -16,6 +16,7 @@ class SendGiftController extends Controller
 	
     public function index()
     {
+		return response('非常抱歉，禮盒目前暫停供應。');
     	$giftProduct = Products::where('slug','30007')->firstOrFail();
     	return view('sendGift.index',['giftProduct'=>$giftProduct]);
     }
