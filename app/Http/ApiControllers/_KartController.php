79cfc410566d1187a46455bf00ae88aa7b4aeb2a
beta0221 <beta0221@gmail.com>
2023-04-15 16:08:06 +0800

    購物車 api

--- app/Http/ApiControllers/_KartController.php
+++ app/Http/ApiControllers/_KartController.php
@@ -3,7 +3,26 @@
 namespace App\Http\ApiControllers;
 
 use App\Http\Controllers\kartController;
+use App\ProductCategory;
+use App\Products;
+use App\sessionCart;
 
 class _KartController extends kartController {
+
+
+    public function items() {
+
+        $ip = request()->ip();
+        $idArray = sessionCart::productsId($ip);
+        
+        $products = Products::select(['id','name','category_id'])->whereIn('id',$idArray)->get();
+
+        foreach ($products as $product) {
+            $product->imgUrl = ProductCategory::getDetailImgUrl($product->category_id);
+        }
+
+        return response($products);
+
+    }
 
 }
