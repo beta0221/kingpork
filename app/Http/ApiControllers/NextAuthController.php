fcbfd8f832c94dd3f8de721f1adbc34f745d4248
beta0221 <beta0221@gmail.com>
2023-04-26 01:26:30 +0800

    登出 註冊 api

--- app/Http/ApiControllers/NextAuthController.php
+++ app/Http/ApiControllers/NextAuthController.php
@@ -4,16 +4,46 @@ namespace App\Http\ApiControllers;
 
 namespace App\Http\ApiControllers;
 use App\Http\Controllers\Controller;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class NextAuthController extends Controller {
 
+    /**
+     * 註冊
+     */
+    public function signup(Request $request) {
 
-    public function login(Request $request)
-    {
-    	//validate the form data
+        //validate the form data
+    	$this->validate($request,[
+    		'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'phone' => 'required|max:255|string',
+    	]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'phone' => $request->phone,
+        ]);
+
+        $user->save();
+
+        return response([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+    /**
+     * 登入 
+     */
+    public function login(Request $request) {
+    	
+        //validate the form data
     	$this->validate($request,[
     		'email' => 'required|email',
     		'password' => 'required|min:4'
@@ -23,7 +53,7 @@ class NextAuthController extends Controller {
         $credentials = request(['email', 'password']);
 
         if(!Auth::attempt($credentials)) {
-            return response()->json([
+            return response([
                 'message' => 'Unauthorized'
             ], 401);
         }
@@ -34,7 +64,7 @@ class NextAuthController extends Controller {
         $token->expires_at = Carbon::now()->addWeeks(1);
         $token->save();
 
-        return response()->json([
+        return response([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
             'expires_at' => Carbon::parse(
@@ -43,6 +73,19 @@ class NextAuthController extends Controller {
         ]);
     		
     }
+
+    /**
+     * 登出
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
 
     public function user(Request $request)
     {
