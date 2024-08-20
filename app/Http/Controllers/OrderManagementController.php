082fc6357f1ac9ea7e536527c0d509e16b81f93c
beta0221 <beta0221@gmail.com>
2024-08-20 15:40:39 +0800

    bugfix

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -825,10 +825,13 @@ class OrderManagementController extends Controller
         $data = json_decode($request->excel_data, true);
         $excelOrder = new ExcelHelper($data);
 
+        
         if($bills = $excelOrder->validateOrderNum($request->kol)) {
-            return response()->json([
-                '錯誤：已存在單號' => $bills
-            ]);
+            if (empty($bills)) {
+                return response()->json([
+                    '錯誤：已存在單號' => $bills
+                ]);
+            }
         }
 
         $excelOrder->save($request->kol);
