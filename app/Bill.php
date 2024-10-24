6691ca8f4167211c76aea2528339f218a60e65f6
beta0221 <beta0221@gmail.com>
2024-10-24 18:23:50 +0800

    訂單列表 api 狀態欄位 資料

--- app/Bill.php
+++ app/Bill.php
@@ -37,7 +37,8 @@ class Bill extends Model
     const CARRIER_ID_FAMILY_MART = 1;
     const CARRIER_FAMILY_MART = '全家冷凍超取';
 
-    public function shipmentName() {
+    public function shipmentName()
+    {
         switch ($this->shipment) {
             case static::SHIPMENT_READY:
                 return "可準備";
@@ -52,7 +53,8 @@ class Bill extends Model
         }
     }
 
-    public static function getAllCarriers(){
+    public static function getAllCarriers()
+    {
         $carriers = [
             static::CARRIER_ID_BLACK_CAT => static::CARRIER_BLACK_CAT,
             static::CARRIER_ID_FAMILY_MART => static::CARRIER_FAMILY_MART,
@@ -60,24 +62,29 @@ class Bill extends Model
         return $carriers;
     }
 
-    public function familyStore(){
-        return $this->hasOne('App\FamilyStore','bill_id','id');
+    public function familyStore()
+    {
+        return $this->hasOne('App\FamilyStore', 'bill_id', 'id');
     }
 
-    public function paymentLogs(){
+    public function paymentLogs()
+    {
         return $this->hasMany('App\PaymentLog');
     }
 
-    public function billItems(){
-        return $this->hasMany('App\BillItem','bill_id','id');
-    }
-    
-    /** 產生訂單流水號 */
-    public static function genMerchantTradeNo($index = null) {
-        return time() . rand(10,99) . (is_null($index) ? '' : $index);
+    public function billItems()
+    {
+        return $this->hasMany('App\BillItem', 'bill_id', 'id');
     }
 
-    public static function insert_row($user_id,$user_name,$bill_id,$useBonus,$total,$getBonus,Request $request){
+    /** 產生訂單流水號 */
+    public static function genMerchantTradeNo($index = null)
+    {
+        return time() . rand(10, 99) . (is_null($index) ? '' : $index);
+    }
+
+    public static function insert_row($user_id, $user_name, $bill_id, $useBonus, $total, $getBonus, Request $request)
+    {
         $bill = new Bill;
         $bill->user_id = $user_id;
         $bill->bill_id = $bill_id;
@@ -97,7 +104,7 @@ class Bill extends Model
         $bill->ship_county = $request->ship_county;
         $bill->ship_district = $request->ship_district;
         $bill->ship_address = $request->ship_address;
-        if($request->carrier_id == Bill::CARRIER_ID_FAMILY_MART){
+        if ($request->carrier_id == Bill::CARRIER_ID_FAMILY_MART) {
             $bill->ship_county = null;
             $bill->ship_district = null;
             $bill->ship_address = $request->store_name;
@@ -112,16 +119,19 @@ class Bill extends Model
         $bill->ship_memo = $request->ship_memo;
         $bill->pay_by = $request->ship_pay_by;
         $bill->carrier_id = $request->carrier_id;
-        if($request->ship_pay_by == 'cod'){
+        if ($request->ship_pay_by == 'cod') {
             $bill->pay_by = '貨到付款';
         }
         $bill->save();
         return $bill;
     }
 
-    public function getErpCustomerId() {
+    public function getErpCustomerId()
+    {
 
-        if ($this->ship_receipt == 3) { return null; }
+        if ($this->ship_receipt == 3) {
+            return null;
+        }
 
         switch ($this->pay_by) {
             case static::PAY_BY_CREDIT:
@@ -146,23 +156,26 @@ class Bill extends Model
         return null;
     }
 
-    public function getErpDeparmentName() {
+    public function getErpDeparmentName()
+    {
         return static::getVendorName($this->kol);
     }
 
-    public static function getVendorName($key) {
+    public static function getVendorName($key)
+    {
         switch ($key) {
-        case "waymay":
-            return "為美";
-        case "bawmami":
-            return "寶媽咪";
-        default:
-            return "官網";
+            case "waymay":
+                return "為美";
+            case "bawmami":
+                return "寶媽咪";
+            default:
+                return "官網";
         }
     }
 
     /** 指定到貨時段 代號 */
-    public function getShiptimeId() {
+    public function getShiptimeId()
+    {
         switch ($this->ship_time) {
             case '13點前':
             case '1':
@@ -177,57 +190,75 @@ class Bill extends Model
         }
     }
 
-    public function products(){
-        if(is_null($this->item)){ return $this->billItems()->get(); }
+    public function products()
+    {
+        if (is_null($this->item)) {
+            return $this->billItems()->get();
+        }
 
-        $items = json_decode($this->item,true);
+        $items = json_decode($this->item, true);
         $slugArray = [];
         $quantityDict = [];
         foreach ($items as $item) {
             $quantityDict[$item['slug']] = $item['quantity'];
             $slugArray[] = $item['slug'];
         }
-        $products = Products::whereIn('slug',$slugArray)->get();
+        $products = Products::whereIn('slug', $slugArray)->get();
         foreach ($products as $product) {
-            if(!isset($quantityDict[$product->slug])){ continue; }
+            if (!isset($quantityDict[$product->slug])) {
+                continue;
+            }
             $product->quantity = $quantityDict[$product->slug];
         }
         return $products;
     }
 
     /**發送這筆訂單可獲得的紅利點數給購買人 */
-    public function sendBonusToBuyer(){
-        if(empty($this->user_id)){ return; }
+    public function sendBonusToBuyer()
+    {
+        if (empty($this->user_id)) {
+            return;
+        }
         $user = User::find($this->user_id);
-        $user->updateBonus($this->get_bonus,false);
+        $user->updateBonus($this->get_bonus, false);
     }
 
     /**更新出貨狀態 */
-    public function updateShipment(int $shipment){
+    public function updateShipment(int $shipment)
+    {
         $this->shipment = $shipment;
         $this->save();
     }
 
     /**取得綠界金流的付款資訊 */
-    public function getPaymentInfo($type = null){
+    public function getPaymentInfo($type = null)
+    {
         $ecpay = new ECPay($this);
-        if(!$data = $ecpay->getPaymentInfo()){ return null; }
-        if(!$type){ return $data; }
-        if(!isset($data[$type])){ return null; }
+        if (!$data = $ecpay->getPaymentInfo()) {
+            return null;
+        }
+        if (!$type) {
+            return $data;
+        }
+        if (!isset($data[$type])) {
+            return null;
+        }
 
-        return (object)$data[$type];
+        return (object) $data[$type];
     }
 
     /**是否為貨到收款的訂單 */
-    public function isCodGroup(){
-        if($this->pay_by == Bill::PAY_BY_COD || $this->pay_by == Bill::PAY_BY_FAMILY){
+    public function isCodGroup()
+    {
+        if ($this->pay_by == Bill::PAY_BY_COD || $this->pay_by == Bill::PAY_BY_FAMILY) {
             return true;
         }
         return false;
     }
 
     /**下階段出貨狀態 */
-    public function nextShipmentPhase(){
+    public function nextShipmentPhase()
+    {
 
         // 網紅收單 && 尚未出貨 => 直接下階段 不需做任何處理
         if ($this->pay_by == static::PAY_BY_KOL && $this->shipment < 2) {
@@ -237,20 +268,20 @@ class Bill extends Model
         }
 
         //不是貨到收款 又還沒付錢的話
-        if(!$this->isCodGroup() AND $this->status != 1){
+        if (!$this->isCodGroup() and $this->status != 1) {
             return;
         }
 
         if ($this->shipment == 0) {
             $this->shipment = 1;
-            if ($this->isCodGroup()) {//如果是貨到付款
-                dispatch(new ECPayInvoice($this,ECPayInvoice::TYPE_ISSUE)); //開立發票
-            }  
-        }elseif ($this->shipment == 1) {
+            if ($this->isCodGroup()) { //如果是貨到付款
+                dispatch(new ECPayInvoice($this, ECPayInvoice::TYPE_ISSUE)); //開立發票
+            }
+        } elseif ($this->shipment == 1) {
             $this->shipment = 2;
-            if ($this->isCodGroup()) {//如果是貨到付款->累計紅利    
-                if($user = User::find($this->user_id)){
-                    $user->updateBonus((int)$this->get_bonus,false);
+            if ($this->isCodGroup()) { //如果是貨到付款->累計紅利    
+                if ($user = User::find($this->user_id)) {
+                    $user->updateBonus((int) $this->get_bonus, false);
                 }
             }
         }
@@ -266,21 +297,22 @@ class Bill extends Model
     }
 
     /**訂單作廢（結案） */
-    public function voidBill(){        
-        
+    public function voidBill()
+    {
+
         //回補紅利
-        if($this->bonus_use != 0){
+        if ($this->bonus_use != 0) {
             $amount = $this->bonus_use * 50;
-            if($user = User::find($this->user_id)){
-                $user->updateBonus($amount,false);
+            if ($user = User::find($this->user_id)) {
+                $user->updateBonus($amount, false);
             }
         }
 
         //扣除紅利
-        if($this->get_bonus != 0){
-            if(($this->isCodGroup() && $this->shipment == 2) || $this->status == 1){
-                if($user = User::find($this->user_id)){
-                    $user->updateBonus((int)$this->get_bonus);
+        if ($this->get_bonus != 0) {
+            if (($this->isCodGroup() && $this->shipment == 2) || $this->status == 1) {
+                if ($user = User::find($this->user_id)) {
+                    $user->updateBonus((int) $this->get_bonus);
                 }
             }
         }
@@ -292,12 +324,63 @@ class Bill extends Model
     // 自訂格式資料
     public function format()
     {
+
+        $pay_by = "-";
+        switch ($this->pay_by) {
+            case static::PAY_BY_ATM:
+                $pay_by = "ATM轉帳";
+                break;
+            case static::PAY_BY_COD:
+                $pay_by = static::PAY_BY_COD;
+                break;
+            case static::PAY_BY_CREDIT:
+                $pay_by = "信用卡";
+                break;
+            default:
+                break;
+        }
+
+        $status = "-";
+        $isStatusComplete = false;
+        if ($this->status == 1) {
+            $status = "已付款";
+            $isStatusComplete = true;
+        } else if ($this->pay_by == static::PAY_BY_COD) {
+            $status = "-";
+        } else {
+            $status = "未付款";
+        }
+
+        $shipment = "-";
+        $isShipmentComplete = false;
+        switch ($this->shipment) {
+            case 1:
+                $shipment = "準備中";
+                break;
+            case 2:
+                $shipment = "已出貨";
+                $isShipmentComplete = true;
+                break;
+            case 3:
+                $shipment = "已取消";
+                break;
+            default:
+                break;
+        }
+
+        $cancelable = ($this->status != 1 && $this->shipment == 0);
+
         return [
             'bill_id' => $this->bill_id,
             'bonus_use' => $this->bonus_use,
             'price' => $this->price,
+            'pay_by' => $pay_by,
+            'status' => $status,
+            'isStatusComplete' => $isStatusComplete,
+            'shipment' => $shipment,
+            'isShipmentComplete' => $isShipmentComplete,
+            'cancelable' => $cancelable,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
         ];
     }
-    
 }
