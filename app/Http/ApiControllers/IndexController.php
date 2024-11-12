1d3d4ea4586da3a93411bc76710af1a134d91076
beta0221 <beta0221@gmail.com>
2024-11-12 21:02:22 +0800

    聯絡我們 api

--- app/Http/ApiControllers/IndexController.php
+++ app/Http/ApiControllers/IndexController.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ProductCategory;
 use App\Banner;
-use App\sessionCart;
-use Symfony\Component\HttpFoundation\Response;
+use App\Contact;
+use Validator;
 
-class IndexController extends Controller {
 
-    public function banners() {
-        $banners = Banner::select(['id','image','link','alt'])->where('public',1)->orderBy('sort','desc')->get();
+class IndexController extends Controller
+{
+
+    public function banners()
+    {
+        $banners = Banner::select(['id', 'image', 'link', 'alt'])->where('public', 1)->orderBy('sort', 'desc')->get();
 
         foreach ($banners as $banner) {
             $host = config('app.url');
@@ -25,7 +28,7 @@ class IndexController extends Controller {
     public function categories()
     {
         $idArray = ProductCategory::$publicIdArray;
-        $_cats = ProductCategory::select(['id','name','slug'])->whereIn('id',$idArray)->get();
+        $_cats = ProductCategory::select(['id', 'name', 'slug'])->whereIn('id', $idArray)->get();
 
         $indexDict = [];
         foreach ($_cats as $index => $cat) {
@@ -36,11 +39,43 @@ class IndexController extends Controller {
 
         $cats = [];
         foreach ($idArray as $id) {
-            if (!isset($indexDict[$id])) { continue; }
+            if (!isset($indexDict[$id])) {
+                continue;
+            }
             $cats[] = $_cats[$indexDict[$id]];
         }
 
         return Response($cats);
     }
 
+    public function contact(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|E-mail',
+            'title' => 'required',
+            'text' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //Ｃaptcha Validation
+
+        // sleep(5);
+
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'title' => $request->title,
+            'message' => $request->text
+        ]);
+
+        return response([
+            'success' => true,
+            'message' => '訊息已成功送出，我們將會儘速回覆您。'
+        ]);
+    }
 }
