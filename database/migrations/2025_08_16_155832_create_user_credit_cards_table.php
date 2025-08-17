175bb0494d814b9d4fb4ee67fc1957e521b2ef86
beta0221 <beta0221@gmail.com>
2025-08-17 12:48:33 +0800

    修正 migration expiry_month expiry_year 為 nullable

--- database/migrations/2025_08_16_155832_create_user_credit_cards_table.php
+++ database/migrations/2025_08_16_155832_create_user_credit_cards_table.php
@@ -19,8 +19,8 @@ class CreateUserCreditCardsTable extends Migration
             $table->string('card_alias')->comment('卡片別名');
             $table->string('masked_card_number')->comment('遮罩卡號');
             $table->string('card_holder_name')->comment('持卡人姓名');
-            $table->tinyInteger('expiry_month')->comment('到期月份');
-            $table->integer('expiry_year')->comment('到期年份');
+            $table->tinyInteger('expiry_month')->nullable()->comment('到期月份');
+            $table->integer('expiry_year')->nullable()->comment('到期年份');
             $table->string('card_brand')->comment('卡片品牌');
             $table->string('ecpay_member_id')->nullable()->comment('ECPay會員編號');
             $table->boolean('is_default')->default(false)->comment('是否為預設卡片');
